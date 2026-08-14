7b86cdac880d6a98c8586fcecc0e4aeff9da8645
delete nullptr <162191486+de1etenu11ptr@users.noreply.github.com>
2026-08-14 15:44:31 +0200

    feat: added functions for checking whether player is in event eligible location

---
+++ app/src/frontend/js/events.js
@@ -0,0 +1,27 @@
+import { distance } from '../utils/general.js'
+import { get_player_location } from '../utils/geolocation.js'
+
+function get_event_location(game_event) {
+	return [game_event.latitude, game_event.longitude]
+}
+
+async function get_player_event_eligibality(game_event) {
+	try {
+		var player_loc = await get_player_location()
+		var event_loc = get_event_location(game_event)
+
+		var d = distance(
+			player_loc[0],
+			player_loc[1],
+			event_loc[0],
+			event_loc[1]
+		)
+
+		if (d < game_event.radius_meters)
+			return true;
+		else
+			return false;
+	} catch (err) {
+		return false
+	}
+}
---
+++ app/src/frontend/js/general.js
@@ -0,0 +1,33 @@
+// Source: 'https://en.wikipedia.org/wiki/Haversine_formula'
+// returns: distance between two points in meters
+function calculate_haversine_distance(lat1, lon1, lat2, lon2) {
+	const R = 6371000
+	const to_radians = (degree) => (degree * Math.PI) / 180
+
+	const d_lat = to_radians(lat2 - lat1)
+	const d_lon = to_radians(lon2 - lon1)
+
+	const r_lat1 = to_radians(lat1)
+	const r_lat2 = to_radians(lat2)
+
+	// Haversine formula
+	const a =
+		Math.pow(Math.sin(d_lat / 2), 2) +
+		Math.sin(d_lon / 2) *
+			Math.sin(d_lon / 2) *
+			Math.cos(r_lat1) *
+			Math.cos(r_lat2)
+
+	const c = 2 * Math.asin(Math.min(1, Math.sqrt(a)))
+
+	return R * c
+}
+
+function distance(loc1, loc2) {
+	return calculate_haversine_distance(
+		loc1.latitude,
+		loc1.longitude,
+		loc2.latitude,
+		loc2.longitude
+	)
+}
