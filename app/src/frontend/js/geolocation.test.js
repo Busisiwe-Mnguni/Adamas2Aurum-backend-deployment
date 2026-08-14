6a548f9a232a1c51bbf6715b0b34ca65ced7aaf2
delete nullptr <162191486+de1etenu11ptr@users.noreply.github.com>
2026-08-14 15:48:25 +0200

    chore: snakecase

--- app/src/frontend/js/geolocation.test.js
+++ app/src/frontend/js/geolocation.test.js
@@ -3,18 +3,18 @@ import { jest } from '@jest/globals';
 import { get_player_location } from './geolocation.js'
 
 describe('get_player_location', () => {
-	const originalGeolocation = global.navigator?.geolocation
+	const original_geolocation = global.navigator?.geolocation
 
 	afterEach(() => {
 		jest.restoreAllMocks()
 		Object.defineProperty(global.navigator, 'geolocation', {
-			value: originalGeolocation,
+			value: original_geolocation,
 			configurable: true,
 		})
 	})
 
 	test('resolves with [latitude, longitude] on success', async () => {
-		const mockGetCurrentPosition = jest.fn((success) => {
+		const mock_get_current_position = jest.fn((success) => {
 			success({
 				coords: {
 					latitude: 12.3456,
@@ -24,12 +24,12 @@ describe('get_player_location', () => {
 		})
 
 		Object.defineProperty(global.navigator, 'geolocation', {
-			value: { getCurrentPosition: mockGetCurrentPosition },
+			value: { getCurrentPosition: mock_get_current_position },
 			configurable: true,
 		})
 
 		const result = await get_player_location()
 		expect(result).toEqual([12.3456, -65.4321])
-		expect(mockGetCurrentPosition).toHaveBeenCalledTimes(1)
+		expect(mock_get_current_position).toHaveBeenCalledTimes(1)
 	})
 })
