193eba01eef1819ca88a1b3be7b316db6974d51d
delete nullptr <162191486+de1etenu11ptr@users.noreply.github.com>
2026-08-20 16:46:31 +0200

    feat: persistent session info when server restarts

--- app/src/backend/server.js
+++ app/src/backend/server.js
@@ -1,11 +1,13 @@
 import 'dotenv/config'
 import express from 'express'
 import session from 'express-session'
+import mySQLSession from 'express-mysql-session'
 import cors from 'cors'
 
 import pool from './utils/db.js'
 import event_routes from './routes/events.js'
 import card_routes from './routes/cards.js'
+import battle_routes from './routes/battle.js'
 import auth_routes from './routes/auth.js'
 import { execute_sql_script } from './utils/sql_utils.js'
 
@@ -14,6 +16,7 @@ const PORT = process.env.PORT || 3000
 
 //CORS
 const allowed_origins = ['http://localhost:8055']
+app.use(express.json())
 app.use(
 	cors({
 		origin: (origin, callback) => {
@@ -27,15 +30,26 @@ app.use(
 	})
 )
 
+const MySQLStore = mySQLSession(session)
+const sessionStore = new MySQLStore(
+	{
+		clearExpired: true,
+		checkExpirationInterval: 900000, // 15 mins
+		expiration: 86400000, // 24 hrs
+	},
+	pool
+)
 app.use(
 	session({
+		key: 'a2a-session-key',
 		secret: process.env.SESSION_SECRET || 'a2a-dev-secret',
+		store: sessionStore,
 		resave: false,
 		saveUninitialized: false,
 		cookie: {
 			httpOnly: true,
 			secure: false, // set true in production with HTTPS
-			maxAge: 1000 * 60 * 60 * 8, // 8 hours
+			maxAge: 1000 * 60 * 60 * 24, // 8 hours
 		},
 	})
 )
@@ -45,11 +59,10 @@ app.use((req, res, next) => {
 	next()
 })
 
-app.use(express.json())
-
 app.use('/api/auth', auth_routes)
 app.use('/api/events', event_routes)
 app.use('/api/cards', card_routes)
+app.use('/api/battles', battle_routes)
 
 app.get('/api/health', async (req, res) => {
 	try {
@@ -75,8 +88,8 @@ async function view_database() {
 }
 
 try {
-	await initialize_database()
-	await seed_database()
+	//await initialize_database()
+	//await seed_database()
 	await view_database()
 } catch (err) {
 	console.error('error: ', err.message)
