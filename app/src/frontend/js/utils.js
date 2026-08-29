76e10308547ccf4591684ec7b437d8a2cc3dc28a
delete nullptr <162191486+de1etenu11ptr@users.noreply.github.com>
2026-08-29 22:22:55 +0200

    fix: bad import

--- app/src/frontend/js/utils.js
+++ app/src/frontend/js/utils.js
@@ -1,7 +1,6 @@
 import { API_BASE } from './constants.js'
 
 export const EVENTS_API = `${API_BASE}/api/events`
-
 let _toastTimer
 export function showToast(msg, type = 'success') {
 	const toast = document.getElementById('toast')
