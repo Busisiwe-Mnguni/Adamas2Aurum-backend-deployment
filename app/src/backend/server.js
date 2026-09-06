36fae9fbf60c215f0c21a9ca88a0fff2823ffe52
delete nullptr <162191486+de1etenu11ptr@users.noreply.github.com>
2026-09-06 04:11:02 +0200

    fix: fixed allowed origins data type for process env

--- app/src/backend/server.js
+++ app/src/backend/server.js
@@ -44,7 +44,7 @@ const PORT = process.env.PORT || 3000
 
 app.use(express.json())
 
-const allowed_origins = process.env.FRONTEND_URL || [
+const allowed_origins = [process.env.FRONTEND_URL] || [
 	'http://localhost:8055',
 	'http://localhost:5173',
 	'http://127.0.0.1:5173',
