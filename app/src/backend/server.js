fd798d2461c44ba70c37d940820a1b7774b9e56c
delete nullptr <162191486+de1etenu11ptr@users.noreply.github.com>
2026-09-06 17:32:39 +0200

    chore: format

--- app/src/backend/server.js
+++ app/src/backend/server.js
@@ -44,14 +44,13 @@ const PORT = process.env.PORT || 3000
 app.use(express.json())
 
 var allowed_origins = [
-		'http://localhost:8055',
-		'http://localhost:5173',
-		'http://127.0.0.1:5173',
-		'http://localhost:3000',
-		'http://127.0.0.1:3000',
-	]
-if (process.env.FRONTEND_URL)
-	allowed_origins.push(process.env.FRONTEND_URL)
+	'http://localhost:8055',
+	'http://localhost:5173',
+	'http://127.0.0.1:5173',
+	'http://localhost:3000',
+	'http://127.0.0.1:3000',
+]
+if (process.env.FRONTEND_URL) allowed_origins.push(process.env.FRONTEND_URL)
 
 app.use(
 	cors({
